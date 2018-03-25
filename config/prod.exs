228b757bdad2f14cf802bb559a03aaa814b44ffd
Pardeep Singh <pardeep.kaler1993@gmail.com>
2018-03-25 15:57:11 -0400

    changes

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :checkers, CheckersWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "checkers.deepwebdev.site", port: 80],
+  url: [host: "", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
